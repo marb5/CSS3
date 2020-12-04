9e15f1ca5b010771478a1bbadcd5331f00010244
Marcin <mbielen1995@gmail.com>
2020-12-04 18:04:54 +0100

    Add animation

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $(".panel").click(function(){
         $(this).toggleClass("panel_clicked");
     });
+    $("#stopResumeAnimation").click(function(){
+        $(".panel2").toggleClass("stopAnimation");
+    });
 });
